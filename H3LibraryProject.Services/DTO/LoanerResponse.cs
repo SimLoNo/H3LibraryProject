773ba9399c12c6e189b15e5f36506747a68a033e
RCSch <robinschneidermann@gmail.com>
2022-05-20 09:56:21 +0200

    Lavet nogle responses. Knap halvejs.

--- H3LibraryProject.Services/DTO/LoanerResponse.cs
+++ H3LibraryProject.Services/DTO/LoanerResponse.cs
@@ -1,6 +1,25 @@
-﻿namespace H3LibraryProject.API.DTOs
+﻿using System.Collections.Generic;
+
+namespace H3LibraryProject.API.DTOs
 {
     public class LoanerResponse
     {
+        public int LoanerId {get; set;}
+        public string LoanerName {get; set;}
+
+        public List<LoanerLoanResponse> Loans { get; set; } = new List<LoanerLoanResponse>();
+        //Sådan har den virket i mit program tidligere.
+
+    }
+
+    public class LoanerLoanResponse
+    {
+        public int LoanerId { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public int RYear { get; set; }
+        public int Pages { get; set; }
+        public int PublisherId { get; set; }
+        public int GenreId { get; set; }
     }
 }
