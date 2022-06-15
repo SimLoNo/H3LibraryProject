8994ebfbfac82925981aaeab77142d39872eb513
robinschneidermann@gmail.com <robinschneidermann@gmail.com>
2022-06-15 11:23:18 +0200

    følger www.youtube.com/watch?v=eMJ5spB3P1c

--- H3LibraryProject.Client/src/app/login/login.component.ts
+++ H3LibraryProject.Client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{FormGroup, FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public loginForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username:[''],
+      password:['']
+    })
   }
 
 }
