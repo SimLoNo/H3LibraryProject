b4b65479747caf538ffa29c003b11d6d159ef948
robinschneidermann@gmail.com <robinschneidermann@gmail.com>
2022-06-16 12:14:59 +0200

    De sidste par ServiceTests.

---
+++ H3LibraryProject.Tests/ServiceTests/MaterialServiceTests.cs
@@ -0,0 +1,244 @@
+﻿using H3LibraryProject.API.DTOs;
+using H3LibraryProject.Repositories.Database;
+using H3LibraryProject.Repositories.Repositories;
+using H3LibraryProject.Services.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace H3LibraryProject.Tests.ServiceTests
+{
+    public class MaterialServiceTests
+    {
+        private readonly MaterialService _service;
+        private readonly Mock<IMaterialRepository> _mockRepository = new();
+
+        public MaterialServiceTests()
+        {
+            _service = new(_mockRepository.Object);
+        }
+        [Fact]
+        public async void CreateMaterial_ShouldReturnNMaterialResponse_whenMaterialIsCreated()
+        {
+            //Arrange
+            int id = 1;
+            Material material = new()
+            {
+                MaterialId = id,
+                TitleId = id,
+            };
+            MaterialRequest request = new()
+            {
+                TitleId = id,
+            };
+
+            _mockRepository
+                .Setup(x => x.CreateMaterial(It.IsAny<Material>()))
+                .ReturnsAsync(material);
+            //Act
+            var result = await _service.CreateMaterial(request);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<MaterialResponse>(result);
+        }
+
+
+        [Fact]
+        public async void CreateMaterial_ShouldReturnNull_WhenMaterialIsNotCreated()
+        {
+            //Arrange
+            MaterialRequest request = new()
+            {
+                TitleId = 1
+            };
+
+            _mockRepository
+                .Setup(x => x.CreateMaterial(It.IsAny<Material>()))
+                .ReturnsAsync(() => null);
+            //Act
+            var result = await _service.CreateMaterial(request);
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void DeleteMaterial_ShouldReturnMaterialResponse_WhenMaterialIsDeleted()
+        {
+            //Arrange
+            int id = 1;
+            Material material = new()
+            {
+                MaterialId = id,
+                TitleId = id,
+            };
+
+            _mockRepository
+                .Setup(x => x.DeleteMaterial(It.IsAny<int>()))
+                .ReturnsAsync(material);
+            //Act
+            var result = await _service.DeleteMaterial(id);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<MaterialResponse>(result);
+            Assert.Equal(id, result.MaterialId);
+        }
+
+        [Fact]
+        public async void DeleteMaterial_ShouldReturnNull_WhenMaterialIsNotDeleted()
+        {
+            //Arrange
+            int id = 1;
+
+            _mockRepository
+                .Setup(x => x.DeleteMaterial(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+
+            //Act
+            var result = await _service.DeleteMaterial(id);
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void GetAllMaterials_ShouldReturnListOfMaterialResponse_WhenMaterialsExist()
+        {
+            //Arrange
+            int id = 1;
+            List<Material> materials = new()
+            {
+                new()
+                {
+                    MaterialId = id,
+                    TitleId = id,
+                    Home = true,
+                    LocationId = id
+                },
+                new()
+                {
+                    MaterialId = id + 1,
+                    TitleId = id + 2,
+                    Home = true,
+                    LocationId = id
+                }
+            };
+
+            _mockRepository
+                .Setup(l => l.GetAllMaterials())
+                .ReturnsAsync(materials);
+            //Act
+            var result = await _service.GetAllMaterials();
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<MaterialResponse>>(result);
+            Assert.Equal(materials.Count, result.Count);
+        }
+
+        [Fact]
+        public async void GetAllMaterials_ShouldReturnEmptyListOfMaterialyResponse_WhenNoMaterialsExist()
+        {
+            //Arrange
+            List<Material> materials = new();
+            _mockRepository
+                .Setup(l => l.GetAllMaterials())
+                .ReturnsAsync(materials);
+            //Act
+            var result = await _service.GetAllMaterials();
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<MaterialResponse>>(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async void GetMaterialById_ShouldReturnMaterialyResponse_WhenTheMaterialyExist()
+        {
+            //Arrange
+            int id = 1;
+            Material material = new()
+            {
+                MaterialId = id,
+                TitleId = id,
+                Home = true,
+                LocationId = id
+            };
+
+            _mockRepository
+                .Setup(x => x.GetMaterialById(It.IsAny<int>()))
+                .ReturnsAsync(material);
+            //Act
+            var result = await _service.GetMaterialById(id);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<MaterialResponse>(result);
+            Assert.Equal(id, result.MaterialId);
+        }
+
+        [Fact]
+        public async void GetMaterialById_ShouldReturnNull_WhenTheMaterialDoesNotExist()
+        {
+            //Arrange
+            int id = 1;
+            _mockRepository
+                .Setup(x => x.GetMaterialById(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+            //Act
+            var result = await _service.GetMaterialById(id);
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void UpdateMaterial_ShouldReturnMaterialResponse_WhenTheMaterialIsUpdated()
+        {
+            //Arrange
+            int id = 1;
+            MaterialRequest request = new()
+            {
+                TitleId = 1,
+                Home = true,
+                LocationId = id
+            };
+            Material material = new()
+            {
+                MaterialId = id,
+                TitleId = 1,
+                Home = true,
+                LocationId = id
+            };
+
+            _mockRepository
+                .Setup(x => x.UpdateMaterial(It.IsAny<int>(), It.IsAny<Material>()))
+                .ReturnsAsync(material);
+            //Act
+            var result = await _service.UpdateMaterial(id, request);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<MaterialResponse>(result);
+        }
+
+        [Fact]
+        public async void UpdateMaterial_ShouldReturnNull_WhenTheMaterialIsNotUpdated()
+        {
+            //Arrange
+            int id = 1;
+            MaterialRequest request = new()
+            {
+                TitleId = id,
+                Home = true,
+                LocationId = id
+            };
+
+            _mockRepository
+                .Setup(x => x.UpdateMaterial(It.IsAny<int>(), It.IsAny<Material>()))
+                .ReturnsAsync(() => null);
+            //Act
+            var result = await _service.UpdateMaterial(id, request);
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
---
+++ H3LibraryProject.Tests/ServiceTests/TitleServiceTests.cs
@@ -0,0 +1,288 @@
+﻿using H3LibraryProject.API.DTOs;
+using H3LibraryProject.Repositories.Database;
+using H3LibraryProject.Repositories.Repositories;
+using H3LibraryProject.Services.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace H3LibraryProject.Tests.ServiceTests
+{
+    public class TitleServiceTests
+    {
+        private readonly TitleService _service;
+        private readonly Mock<ITitleRepository> _mockRepository = new();
+
+        public TitleServiceTests()
+        {
+            _service = new(_mockRepository.Object);
+        }
+        [Fact]
+        public async void CreateTitle_ShouldReturnNTitleResponse_whenTitleIsCreated()
+        {
+            //Arrange
+            int id = 1;
+            Title title = new()
+            {
+                TitleId = id,
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                AuthorId = 1,
+                GenreId = 1,
+            };
+            TitleRequest request = new()
+            {
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                GenreId = 1,
+            };
+
+            _mockRepository
+                .Setup(x => x.InsertNewTitle(It.IsAny<Title>()))
+                .ReturnsAsync(title);
+            //Act
+            var result = await _service.CreateTitle(request);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<TitleResponse>(result);
+        }
+
+
+        [Fact]
+        public async void CreateTitle_ShouldReturnNull_WhenTitleIsNotCreated()
+        {
+            //Arrange
+            TitleRequest request = new()
+            {
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                GenreId = 1,
+            };
+
+            _mockRepository
+                .Setup(x => x.InsertNewTitle(It.IsAny<Title>()))
+                .ReturnsAsync(() => null);
+            //Act
+            var result = await _service.CreateTitle(request);
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void DeleteTitle_ShouldReturnTitleResponse_WhenTitleIsDeleted()
+        {
+            //Arrange
+            int id = 1;
+            Title title = new()
+            {
+                TitleId = id,
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                AuthorId = 1,
+                GenreId = 1,
+            };
+
+            _mockRepository
+                .Setup(x => x.DeleteTitle(It.IsAny<int>()))
+                .ReturnsAsync(title);
+            //Act
+            var result = await _service.DeleteTitle(id);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<TitleResponse>(result);
+            Assert.Equal(id, result.TitleId);
+        }
+
+        [Fact]
+        public async void DeleteTitle_ShouldReturnNull_WhenTitleIsNotDeleted()
+        {
+            //Arrange
+            int id = 1;
+
+            _mockRepository
+                .Setup(x => x.DeleteTitle(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+
+            //Act
+            var result = await _service.DeleteTitle(id);
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void GetAllTitles_ShouldReturnListOfTitleResponse_WhenTitlesExist()
+        {
+            //Arrange
+            int id = 1;
+            List<Title> titles = new()
+            {
+                new()
+                {
+                    TitleId = id,
+                    Name = "elmer",
+                    LanguageId = 1,
+                    RYear = 1990,
+                    Pages = 300,
+                    PublisherId = 1,
+                    AuthorId = 1,
+                    GenreId = 1,
+                },
+                new()
+                {
+                    TitleId = id,
+                    Name = "elmer",
+                    LanguageId = 1,
+                    RYear = 1990,
+                    Pages = 300,
+                    PublisherId = 1,
+                    AuthorId = 1,
+                    GenreId = 1,
+                }
+            };
+
+            _mockRepository
+                .Setup(l => l.SelectAllTitles())
+                .ReturnsAsync(titles);
+            //Act
+            var result = await _service.GetAllTitles();
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<TitleResponse>>(result);
+            Assert.Equal(titles.Count, result.Count);
+        }
+
+        [Fact]
+        public async void GetAllTitles_ShouldReturnEmptyListOfTitleyResponse_WhenNoTitlesExist()
+        {
+            //Arrange
+            List<Title> titles = new();
+            _mockRepository
+                .Setup(l => l.SelectAllTitles())
+                .ReturnsAsync(titles);
+            //Act
+            var result = await _service.GetAllTitles();
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<TitleResponse>>(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async void GetTitleById_ShouldReturnTitleResponse_WhenTheTitleyExist()
+        {
+            //Arrange
+            int id = 1;
+            Title title = new()
+            {
+                TitleId = id,
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                AuthorId = 1,
+                GenreId = 1,
+            };
+
+            _mockRepository
+                .Setup(x => x.SelectTitleById(It.IsAny<int>()))
+                .ReturnsAsync(title);
+            //Act
+            var result = await _service.GetTitlesById(id);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<TitleResponse>(result);
+            Assert.Equal(id, result.TitleId);
+        }
+
+        [Fact]
+        public async void GetTitleById_ShouldReturnNull_WhenTheTitleDoesNotExist()
+        {
+            //Arrange
+            int id = 1;
+            _mockRepository
+                .Setup(x => x.SelectTitleById(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+            //Act
+            var result = await _service.GetTitlesById(id);
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void UpdateExistingTitle_ShouldReturnTitleResponse_WhenTheTitleIsUpdated()
+        {
+            //Arrange
+            int id = 1;
+            TitleRequest request = new()
+            {                
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                GenreId = 1,
+            };
+            Title title = new()
+            {
+                TitleId = id,
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                AuthorId = 1,
+                GenreId = 1,
+            };
+
+            _mockRepository
+                .Setup(x => x.UpdateExistingTitle(It.IsAny<int>(), It.IsAny<Title>()))
+                .ReturnsAsync(title);
+            //Act
+            var result = await _service.UpdateTitle(id, request);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<TitleResponse>(result);
+        }
+
+        [Fact]
+        public async void UpdateExistingTitle_ShouldReturnNull_WhenTheTitleIsNotUpdated()
+        {
+            //Arrange
+            int id = 1;
+            TitleRequest request = new()
+            {
+                Name = "elmer",
+                LanguageId = 1,
+                RYear = 1990,
+                Pages = 300,
+                PublisherId = 1,
+                GenreId = 1,
+            };
+
+            _mockRepository
+                .Setup(x => x.UpdateExistingTitle(It.IsAny<int>(), It.IsAny<Title>()))
+                .ReturnsAsync(() => null);
+            //Act
+            var result = await _service.UpdateTitle(id, request);
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
